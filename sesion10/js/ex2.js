83653e692aa4baf0b48ab0bf2534a30602aaa413
thangnvph4303 <vanthang2003az@gmail.com>
2023-01-31 13:24:29 +0700

    update code challeng #2 sesion 10

---
+++ sesion10/js/ex2.js
@@ -0,0 +1,11 @@
+(
+    () => {
+        const header = document.querySelector('h1');
+        header.style.color = 'red';
+
+        document.querySelector('body').addEventListener('click', () => {
+            header.style.color = 'yellow';
+        });
+    }
+)();
+
